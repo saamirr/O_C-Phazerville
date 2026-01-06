1ca9dc118d2e7cc79e8cdf6dcdfb7d9fcfc40a20
Nicholas J. Michalek <djphazer@gmail.com>
2026-01-05 22:34:32 -0500

    new custom USB device name

--- software/src/usb_name.c
+++ software/src/usb_name.c
@@ -7,8 +7,8 @@
 // Edit these lines to create your own name.  The length must
 // match the number of characters in your custom name.
 
-#define MIDI_NAME {'P','e','w','P','e','w','M','I','D','I'}
-#define MIDI_NAME_LEN 10
+#define MIDI_NAME {'P','h','a','z','e','r','v','i','l','l','e'}
+#define MIDI_NAME_LEN 11
 
 // Do not change this part.  This exact format is required by USB.
 
